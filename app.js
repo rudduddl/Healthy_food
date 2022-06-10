1f4172a32f606b14b4c1b39d00a161d0f4097e9f
zeroeightboy <102647024+zeroeightboy@users.noreply.github.com>
2022-06-10 11:30:56 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -18,6 +18,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cookieParser());
+
+let rsp = [
+  {
+    _id: "6288b9ac3d6a655ec5674412",
+    ATT_FILE_NO_MK:
+      "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00017_1.png",
+    RCP_NM: "칼륨 듬뿍 고구마죽",
+  },
+  {
+    _id: "6288b9ac3d6a655ec5674413",
+    ATT_FILE_NO_MK:
+      "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00016_1.png",
+    RCP_NM: "누룽지 두부 계란죽",
+  },
+  {
+    _id: "6288b9ac3d6a655ec5674414",
+    ATT_FILE_NO_MK:
+      "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00009_1.png",
+    RCP_NM: "오색지라시 스시",
+  },
+];
+
 app.use(
   expressSession({
     secret: "HF20220525",
@@ -52,6 +74,7 @@ app.get("/recipe_list", async function (req, res) {
     disease.caution,
     req.query.search
   );
+  rsp = cautionReceipes;
   res.render("recipe_list", {
     favoriteReceipes: favoriteReceipes,
     disease: disease,
@@ -65,9 +88,24 @@ app.get("/recipe_view", async function (req, res) {
     favoriteReceipes = await mongodb.getFavoriteReceipe(req.session.user.id);
   }
   const receipe = await mongodb.getReceipe(req.query.receipe_id);
+
+  console.log(rsp, "gg");
+
+  let randomIndexArray = [];
+  for (i = 0; i < 3; i++) {
+    randomNum = Math.floor(Math.random() * (rsp.length - 1));
+    if (randomIndexArray.indexOf(randomNum) === -1) {
+      randomIndexArray.push(randomNum);
+    } else {
+      i--;
+    }
+  }
+  console.log(rsp.length - 1, randomIndexArray);
   res.render("recipe_view", {
     favoriteReceipes: favoriteReceipes,
     receipe: receipe,
+    cautionReceipes: rsp,
+    number: randomIndexArray,
   });
 });
 
